8da731e129425a4dea8f9152597a3e836c36d203
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-03-18 16:25:56 -0400

    Able to connect to database with simple one-item collection

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
 const mongoose = require("../db/connection");
 
 const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  name: String,
-  pets: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Pet"
-    }
-  ]
+	email: String,
+	password: String,
+	name: String,
+	pets: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Pet"
+		}
+	]
 });
 
 module.exports = mongoose.model("User", UserSchema);

13b76bb284cc48f81a43bbe25816f89135a14fc1
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-03-19 15:03:37 -0400

    UPDATE and DELETE comments

--- controllers/comment.js
+++ controllers/comment.js
@@ -7,9 +7,24 @@ module.exports = {
 			.then(comments => res.json(comments))
 			.catch(err => console.log(err));
 	},
+	create: function(req, res) {
+		Comment.create(req.body)
+			.then(comment => res.json(comment))
+			.catch(err => console.log(err));
+	},
 	show: function(req, res) {
 		Comment.findById(req.params.id)
 			.then(comment => res.json(comment))
+			.catch(err => console.log(err));
+	},
+	update: function(req, res) {
+		Comment.findByIdAndUpdate(req.params.id, req.body)
+			.then(comment => res.json(comment))
+			.catch(err => console.log(err));
+	},
+	delete: function(req, res) {
+		Comment.findByIdAndDelete(req.params.id)
+			.then(() => Comments.find().then(comments => res.json(comments)))
 			.catch(err => console.log(err));
 	}
 };
--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const petController = require("../controllers/api");
 const commentController = require("../controllers/comment");
 
-router.get("/pets/", petController.index);
-router.get("/comments/", commentController.index);
+router.get("/pets", petController.index);
+router.get("/comments", commentController.index);
 router.get("/comments/:id", commentController.show);
-// router.get("/api/bourbons/:name", bourbonController.show);
-// router.post("/api/bourbons/", bourbonController.create);
-// router.put("/api/bourbons/:name", bourbonController.update);
-// router.delete("/api/bourbons/:name", bourbonController.delete);
+router.post("/comments", commentController.create);
+router.put("/comments/:id", commentController.update);
+router.delete("/comments/:id", commentController.delete);
 
 module.exports = router;

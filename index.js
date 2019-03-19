f4fc97bb8ce1c29973a54e5875b8bafef062be6d
Wesley Scholl <clients@konjoweb.com>
2019-03-19 16:25:05 -0400

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const parser = require("body-parser");
-// const cors = require("cors");
+const cors = require("cors");
 // const passport = require("./config/passport")();
 const app = express();
 
 app.use(parser.json());
 // app.use(passport.initialize());
-// app.use(cors());
+app.use(cors());
 
 app.use(require("./routes/index"));
 // app.get("/", function(req, res) {
@@ -16,5 +16,5 @@ app.use(require("./routes/index"));
 app.set("port", process.env.PORT || 3001);
 
 app.listen(app.get("port"), () => {
-	console.log(`Running on PORT: ${app.get("port")}`);
+  console.log(`Running on PORT: ${app.get("port")}`);
 });

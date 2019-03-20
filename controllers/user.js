6f8a4e03040b41ca1cc6f52e7180dd400cc43762
Wesley Scholl <clients@konjoweb.com>
2019-03-20 12:05:56 -0400

    completed back end auth setup

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,64 @@
-const mongoose = require("../db/connection");
+const express = require("express");
+const jwt = require("jwt-simple");
+const passport = require("../config/passport");
+const config = require("../config/config");
+const mongoose = require("../models/User");
+const User = mongoose.model("User");
 
-const UserSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String
+const router = express.Router();
+
+router.post("/signup", (req, res) => {
+  if (req.body.email && req.body.password) {
+    let newUser = {
+      email: req.body.email,
+      password: req.body.password
+    };
+    User.findOne({ email: req.body.email }).then(user => {
+      if (!user) {
+        User.create(newUser).then(user => {
+          if (user) {
+            var payload = {
+              id: newUser.id
+            };
+            var token = jwt.encode(payload, config.jwtSecret);
+            res.json({
+              token: token
+            });
+          } else {
+            res.sendStatus(401);
+          }
+        });
+      } else {
+        res.sendStatus(401);
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
 });
 
-mongoose.model("User", UserSchema);
+router.post("/login", (req, res) => {
+  if (req.body.email && req.body.password) {
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        if (user.password === req.body.password) {
+          var payload = {
+            id: user.id
+          };
+          var token = jwt.encode(payload, config.jwtSecret);
+          res.json({
+            token: token
+          });
+        } else {
+          res.sendStatus(401);
+        }
+      } else {
+        res.sendStatus(401);
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
+});
 
-module.exports = mongoose;
+module.exports = router;

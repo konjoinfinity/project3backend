20b6eba561449dad2a5fbfdf53c06e86cc322aa6
Cathy <Cathy347Le@gmail.com>
2019-03-20 10:40:38 -0400

    update pet model to cinclude default value 0 for licks

--- models/pet.js
+++ models/pet.js
@@ -3,23 +3,26 @@ const mongoose = require("../db/connection");
 //Set up CommentSchema first, then Pet Schema. CommentSchema is a child of the Pet Schema,
 //Also want to export both in module exports
 const CommentSchema = new mongoose.Schema({
-	message: String
+  message: String
 });
 
 const PetSchema = new mongoose.Schema({
-	name: String,
-	description: String,
-	species: {
-		type: String,
-		enum: ["Cat", "Dog"]
-	},
-	profilepicture: String,
-	sociallink: String,
-	licks: Number,
-	comments: [CommentSchema]
+  name: String,
+  description: String,
+  species: {
+    type: String,
+    enum: ["Cat", "Dog"]
+  },
+  profilepicture: String,
+  sociallink: String,
+  licks: {
+    type: Number,
+    default: 0
+  },
+  comments: [CommentSchema]
 });
 
 module.exports = {
-	Pet: mongoose.model("Pet", PetSchema),
-	Comment: mongoose.model("Comment", CommentSchema)
+  Pet: mongoose.model("Pet", PetSchema),
+  Comment: mongoose.model("Comment", CommentSchema)
 };

4bf3a72fba75ad63d3c011ead3c8be9b33b2c631
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-03-20 15:15:14 -0400

    Edit a slight error from previous merge

--- models/pet.js
+++ models/pet.js
@@ -8,25 +8,25 @@ const CommentSchema = new mongoose.Schema({
 		type: Date,
 		default: Date.now()
 	}
-
+});
 
 const PetSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  species: {
-    type: String,
-    enum: ["Cat", "Dog"]
-  },
-  profilepicture: String,
-  sociallink: String,
-  licks: {
-    type: Number,
-    default: 0
-  },
-  comments: [CommentSchema]
+	name: String,
+	description: String,
+	species: {
+		type: String,
+		enum: ["Cat", "Dog"]
+	},
+	profilepicture: String,
+	sociallink: String,
+	licks: {
+		type: Number,
+		default: 0
+	},
+	comments: [CommentSchema]
 });
 
 module.exports = {
-  Pet: mongoose.model("Pet", PetSchema),
-  Comment: mongoose.model("Comment", CommentSchema)
+	Pet: mongoose.model("Pet", PetSchema),
+	Comment: mongoose.model("Comment", CommentSchema)
 };

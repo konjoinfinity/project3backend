14d31beb5f22766082e53e1006453fdd924aecda
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-03-21 09:45:17 -0400

    Sort the ten most recent comments to show

--- controllers/comment.js
+++ controllers/comment.js
@@ -9,6 +9,8 @@ module.exports = {
 	},
 	index: function(req, res) {
 		Comment.find()
+			.sort({ createdAt: -1 })
+			.limit(10)
 			.then(comments => res.json(comments))
 			.catch(err => console.log(err));
 	},

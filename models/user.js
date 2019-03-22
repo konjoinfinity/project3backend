59fa63501e3d027e811ab78f292f78c24c75a788
Wesley Scholl <clients@konjoweb.com>
2019-03-22 17:35:15 -0400

    added bcrypt for auth

--- models/user.js
+++ models/user.js
@@ -1,15 +1,22 @@
 const mongoose = require("../db/connection");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const UserSchema = new mongoose.Schema({
-	email: String,
-	password: String,
-	name: String,
-	pets: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Pet"
-		}
-	]
+  email: String,
+  password: String,
+  name: String,
+  pets: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pet"
+    }
+  ]
+});
+
+UserSchema.pre("save", function(next) {
+  this.password = bcrypt.hashSync(this.password, saltRounds);
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
